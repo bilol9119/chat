8014c6706e6af8bbe0d7957d83e1b4a32c0d0ca9
bilol <bilolnorov7@gmail.com>
2024-06-25 16:29:20 +0500

    chat models and serializers

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,20 @@ import uuid
 from authentication.models import User
 
 
+class Chat(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_num1")
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_num2")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.created_at)
+
+
 class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="chat_two_users")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
 
--- chat/serializers.py
+++ chat/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
-from .models import Message
+from .models import Message, Chat
 from authentication.serializers import UserSerializer
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Message
         fields = ('content', 'created_at', 'user',)
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['user'] = UserSerializer(instance.user).data
+
+class ChatSerializer(serializers.ModelSerializer):
+    messages = MessageSerializer(many=True, read_only=True)
+    user1 = UserSerializer(read_only=True)
+    user2 = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Chat
+        fields = ('user1', 'user2', 'messages', )
